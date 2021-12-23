4b977e24a14644c1a53979eb36d6cdb60974b5a6
leakec <leakec57@gmail.com>
2021-12-23 14:51:03 -0800

    Added wrapper class that waits for elements to be loaded before returning them. This way, we don't have to have sleeps littered everywhere. Also, using the wrapper to keep better track of tabs.

---
+++ my_driver.py
@@ -0,0 +1,40 @@
+import atexit
+from undetected_chromedriver.v2 import Chrome
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+
+class MyDriver(Chrome):
+    
+    def __init__(self,name=None):
+        super().__init__()
+
+        # If an element takes longer than this, exit
+        self._timeout = 10
+
+        # Used to keep track of the tabs that are open
+        self.tabs = {} 
+
+        name = name or "default"
+        self.tabs[name] = self.current_window_handle
+
+        # Register close function
+        atexit.register(self.close)
+
+    def newTab(self,name=None):
+        self.execute_script("window.open('');")
+        name = name or "default"+self._tabIdx
+        self.tabs[name] = self.window_handles[-1]
+        self.switchTab(name)
+
+    def switchTab(self, key: str):
+        if key in self.tabs:
+            k = self.tabs[key]
+        else:
+            raise ValueError(f"key is of type {type(key)}. Only int or str are accepted.")
+        self.switch_to.window(k)
+
+    def get_element(self, *args, **kwargs):
+        """ Finds an element, but after the element is loaded. """
+        element_present = ec.presence_of_element_located(args)
+        return WebDriverWait(self, self._timeout).until(element_present)
+
--- shopping.py
+++ shopping.py
@@ -1,12 +1,9 @@
-import atexit
 import numpy as np
 from time import sleep
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import undetected_chromedriver.v2 as uc
 from Pass import passDict
 from my_items import myItems
+from my_driver import MyDriver
 
 # Get usernames and passwords
 todoistUser = passDict["Todoist"]["username"]
@@ -17,66 +14,42 @@ ralphsPass = passDict["Ralphs"]["password"]
 # Constants
 sleepTime = 2.0
 
-# Create browser session
-browser = uc.Chrome()
-atexit.register(browser.close)
-tabs = {}
-
-# Useful functions
-def newTab(browser):
-    browser.execute_script("window.open('');")
-
-def switchTab(browser,key):
-    if isinstance(key,str):
-        if key in tabs:
-            k = tabs[key]
-    elif isinstance(key,int):
-        k = key
-    else:
-        raise ValueError(f"key is of type {type(key)}. Only int or str are accepted.")
-    browser.switch_to.window(browser.window_handles[k])
-
-# Log into todoist
-browser.get("https://todoist.com")
-tabs["todoist"] = 0
-browser.find_element(By.XPATH,'//*[@id="__next"]/div/main/div[1]/header/nav/div/ul[2]/li[1]/a').click()
-browser.find_element(By.XPATH,'//*[@id="email"]').send_keys(todoistUser)
-browser.find_element(By.XPATH,'//*[@id="password"]').send_keys(todoistPass)
-browser.find_element(By.XPATH,'//*[@id="login_form"]/button').click()
-sleep(5.0)
-
-# Get the Alexa shopping list (synced with todoist)
-browser.find_element(By.XPATH,'//*[@id="projects_list"]/li[4]/table/tbody/tr/td[2]/span').click()
-sleep(sleepTime)
-
-# Get the items from the list
-items = browser.find_elements(By.CSS_SELECTOR, ".task_list_item.task_list_item--project_hidden.task_list_item--shortcuts-enabled")
-sleep(sleepTime)
-
 # Useful functions
 def deleteItem(item):
     item.find_elements(By.CSS_SELECTOR, ".task_checkbox.priority_1")[0].click()
 
+# Create web driver
+browser = MyDriver(name="todoist")
+
+# Log into todoist
+browser.get("https://todoist.com")
+browser.get_element(By.XPATH,'//*[@id="__next"]/div/main/div[1]/header/nav/div/ul[2]/li[1]/a').click()
+browser.get_element(By.XPATH,'//*[@id="email"]').send_keys(todoistUser)
+browser.get_element(By.XPATH,'//*[@id="password"]').send_keys(todoistPass)
+browser.get_element(By.XPATH,'//*[@id="login_form"]/button').click()
+
+# Get the Alexa shopping list (synced with todoist)
+browser.get_element(By.XPATH,'//*[@id="projects_list"]/li[4]/table/tbody/tr/td[2]/span').click()
+
+# Get the items from the list
+items = browser.find_elements(By.CSS_SELECTOR, ".task_list_item.task_list_item--project_hidden.task_list_item--shortcuts-enabled")
+
 food = [j.text.split("\n")[0] for j in items]
 
 # Go to Ralphs
-newTab(browser)
-tabs["ralphs"] = 1
-switchTab(browser,"ralphs")
-sleep(sleepTime)
+browser.newTab(name="ralphs")
 browser.get("https://www.ralphs.com/signin")
-switchTab(browser,0) # Ralphs becomes tab 0 once we get it, not sure why
-browser.find_element(By.XPATH,'//*[@id="SignIn-emailInput"]').send_keys(ralphsUser)
-browser.find_element(By.XPATH,'//*[@id="SignIn-passwordInput"]').send_keys(ralphsPass)
-browser.find_element(By.XPATH,'//*[@id="SignIn-submitButton"]').click() # Not sure why but we need multiple clicks to get past the pop up issue
-sleep(sleepTime)
+browser.switchTab("ralphs") # For some reason get puts us back in the first tab
+browser.get_element(By.XPATH,'//*[@id="SignIn-emailInput"]').send_keys(ralphsUser)
+browser.get_element(By.XPATH,'//*[@id="SignIn-passwordInput"]').send_keys(ralphsPass)
+browser.get_element(By.XPATH,'//*[@id="SignIn-submitButton"]').click() 
 
 # Add items to shopping cart
 addedIdx = np.zeros(len(food),dtype=bool)
 for k,item in enumerate(food):
     keywords = myItems.get(item,[])
     if keywords:
-        sb = browser.find_element(By.XPATH, '//*[@id="SearchBar-input"]')
+        sb = browser.get_element(By.XPATH, '//*[@id="SearchBar-input"]')
         sb.send_keys(item+"\n")
         sleep(sleepTime)
 
@@ -85,7 +58,3 @@ for k,item in enumerate(food):
             if all((x in res.text for x in keywords)):
                 res.find_element(By.CSS_SELECTOR, ".mt-32.mb-12").click()
                 addedIdx[k] = True
-
-
-
-
