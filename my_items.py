fb44d31b5e5ee7fe32ab84efaae836df288a6cc4
leakec <leakec57@gmail.com>
2021-12-26 16:44:12 -0800

    Now items from my items can have a different search term than the item name. * Now clearing search bar in between items.

--- my_items.py
+++ my_items.py
@@ -1,3 +1,8 @@
 myItems = {
-        "chicken": ["Natural Raised Cage Free", "Breast"]
+        "chicken": {"search": "chicken", "keywords": ["Natural Raised Cage Free", "Breast"]},
+        "carl's milk": {"search": "whole milk", "keywords": ["Ralphs", "Whole", "1 gal"]},
+        "nina's milk": {"search": "milk 0% organic", "keywords": ["Fat Free", "0.5 gal"]},
+        "tortilla": {"search": "tortilla", "keywords": ["Whole Wheat", "Flour Tortillas", "24 ct"]},
+        "tortillas": {"search": "tortilla", "keywords": ["Whole Wheat", "Flour Tortillas", "24 ct"]},
 }
+
