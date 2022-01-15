b184ee698c383f0b303ede651f1c2b79f629b651
leakec <leakec57@gmail.com>
2022-01-15 08:01:40 -0800

    Dummy instruction in makefile.

--- recipe_book/docs/recipe_template.md
+++ recipe_book/docs/recipe_template.md
@@ -2,9 +2,4 @@
 
 ## Ingredients
 
-* [ ] 1 28 oz crushed tomatoes
-* [ ] 3 bananas
-
 ## Steps
-1. Do this
-2. Do that
--- recipe_book/recipe_converter.py
+++ recipe_book/recipe_converter.py
@@ -3,26 +3,41 @@
 import yaml
 import click
 
+# Setup click
 @click.command(context_settings=dict(help_option_names=["-h","--help"]))
 @click.pass_context
-@click.argument("yaml-file", type=str)
-@click.argument("output-file", type=str)
+@click.argument("yaml-file", type=str, "Input yaml file that has the recipe data).")
+@click.argument("output-file", type=str, "Output markdown file that contains the formatted recipe..")
+@click.option("--template-file", type=str, help="Recipe template file.", default="recipe_template.md")
 def cli(ctx, yaml_file, output_file, **kwargs):
     kwargs['yaml_file'] = yaml_file
     kwargs['output_file'] = output_file
     return kwargs
 
-if __name__ == '__main__':
-    cfgobj = cli(standalone_mode=False)
+# Get click config
+cfgobj = cli(standalone_mode=False)
 
-    yamlFile = cfgobj["yaml_file"]
-    outputFile = cfgobj["output_file"]
+# Get input and output files
+yamlFile = cfgobj["yaml_file"]
+outputFile = cfgobj["output_file"]
+templateFile = cfgobj["template_file"]
 
-    with open(yamlFile) as f:
-        data = yaml.load(f, Loader=yaml.Loader)
+# Load the data
+with open(yamlFile) as f:
+    data = yaml.load(f, Loader=yaml.Loader)
 
-    title = data['title']
-    ingredients = ["{} {} {} {} {}".format(i.get('amount',''), i.get('units',''), "of" if 'units' in i else "", i['name'], i.get('notes','')).strip() for i in data['ingredients']]
-    steps = data['steps']
+# Format the data
+title = data['title']
+ingredients = ["  * [ ] {} {} {} {} {}\n".format(i.get('amount',''), i.get('units',''), "of" if 'units' in i else "", i['name'], i.get('notes','')).strip() for i in data['ingredients']]
+steps = ["  1. {}\n".format(step) for step in data['steps']]
 
+# Apply data to template
+with open(templateFile, "r") as f:
+    template = f.read()
+
+recipe = template.format(title,ingredients,steps)
+
+# Write recipe
+with open(outputFile, "w") as f:
+    f.write(recipe)
 
